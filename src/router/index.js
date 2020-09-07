fa1e4636b8cf8cd13aa443dc3e381cc758d97d29
Arina IGNATIUC <aignatiuc@pentalog.com>
2020-09-07 23:42:43 +0300

    Add taiwind

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,51 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    component: () => import("../views/Dashboard.vue"),
+    children: [
+      {
+        path: "",
+        name: "",
+        component: {
+          render(c) {
+            return c("router-view");
+          },
+        },
+        children: [
+          {
+            path: "/",
+            name: "Home",
+            component: () => import("../views/Home.vue"),
+          },
+          {
+            path: "/tags",
+            name: "Tags",
+            component: () => import("../views/Tags.vue"),
+          },
+          {
+            path: "/users",
+            name: "Users",
+            component: () => import("../views/Users.vue"),
+          },
+        ],
+      },
+    ],
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    component: () => import("../views/About.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
-  routes
+  routes,
 });
 
 export default router;
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="top-bar"></div>
+    <v-header />
+    <v-main>
+      <router-view />
+    </v-main>
+  </div>
+</template>
+<script>
+import VHeader from "@/components/VHeader.vue";
+
+export default {
+  components: {
+    VHeader,
+  },
+};
+</script>
+
+<style scoped>
+.top-bar {
+  width: 100%;
+  height: 2px;
+  background: var(--orange);
+}
+</style>
