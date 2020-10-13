9507884925d2117acfd721a534bef736a61d1506
aignatiuc <arina.ignatiuc@ee.utm.md>
2020-10-13 12:02:27 +0300

    Add unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,20 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import DashboardHeader from "@/components/dashboard/DashboardHeader.vue";
+import { shallowMount } from "@vue/test-utils";
+import Vue from "vue";
+import Vuetify from "vuetify";
+import VueRouter from "vue-router";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+Vue.use(VueRouter);
+Vue.use(Vuetify);
+
+describe("DashboardHeader.vue", () => {
+  it("render v-dropdown", () => {
+    const wrapper = shallowMount(DashboardHeader, {
+      mocks: {
+        $t: () => {},
+      },
+    });
+    const text = wrapper.find("v-dropdown");
+    expect(text.exists()).toBe(false);
+	});
+});
